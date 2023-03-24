208f6ae8cca7405a85a2c5fb0a4a92594f1a5894
Kennynyamai <kennynyamai73@gmail.com>
2023-03-24 23:39:00 +0300

    fixing the manifest files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
 # create a file in /tmp.
+
 file { '/tmp/school':
-  owner   => 'www-data',
-  group   => 'rowww-data',
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
