13096be6fc80b5c646ca55fdb0bfc3cf1ede715b
Kennynyamai <kennynyamai73@gmail.com>
2023-03-24 23:28:07 +0300

    changes to the second manifest file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
-# install a package
-package {'flask': provider => pip3,: 
-  ensure => '2.1.0',
+#execute a command
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
 }
